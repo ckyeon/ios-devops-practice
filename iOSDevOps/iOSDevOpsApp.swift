88d0dcedd1d6e942842a899f50db6d180c516506
ckyeon <qsky8520@gmail.com>
2023-08-11 14:12:10 +0900

    initial commit

---
+++ iOSDevOps/iOSDevOpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDevOpsApp.swift
+//  iOSDevOps
+//
+//  Created by 조기연 on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct iOSDevOpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
